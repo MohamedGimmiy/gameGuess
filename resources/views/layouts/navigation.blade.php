6d22be50c6be28c88dfdf9dbee7bc1997b5b7d06
MohamedGimmiy <mohamedhussin07@gmail.com>
2025-07-16 21:43:20 +0300

    adding slit modification

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('game.index')" :active="request()->routeIs('game')">
+                    <x-nav-link :href="route('game.index')" :active="request()->routeIs('game.index')">
                         {{ __('Game') }}
                     </x-nav-link>
                 </div>
